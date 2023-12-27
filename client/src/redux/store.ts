77b9b9aad3cc25abdac50de15f4b920bd5ccc827
Ham47Mani <ham47mani@gmail.com>
2023-12-27 22:59:27 +0100

    Add redux persist

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,8 +1,25 @@
-import {configureStore} from "@reduxjs/toolkit"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {combineReducers, configureStore} from "@reduxjs/toolkit"
+import {FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore} from "redux-persist";
 import userReducers from "./user/userSlice";
+import storage from "redux-persist/lib/storage";
+
+// Combine Reducers
+const rootReducers = combineReducers({user: userReducers});
+// Presiste reducers
+const persistConfig = { key: "root", storage, version: 1 }
+const presistedReducer = persistReducer(persistConfig, rootReducers);
 
 export const store = configureStore({
   reducer: {
-    user: userReducers
-  }
+    user: presistedReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
